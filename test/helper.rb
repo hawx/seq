9086176e69f31b4f0bb4d46180263d394f96edb8
Joshua Hawxwell <m@hawx.me>
2011-07-19 10:11:53 +0100

    Created directories and files

---
+++ test/helper.rb
@@ -0,0 +1,7 @@
+$: << File.dirname(__FILE__)
+$: << File.dirname(__FILE__) + '/../lib'
+
+require 'seq'
+
+require 'minitest/unit'
+require 'minitest/autorun'
---
+++ test/seq_test.rb
@@ -0,0 +1,6 @@
+$: << File.dirname(__FILE__)
+require 'helper'
+
+class SeqTest < MiniTest::Unit::TestCase
+
+end
